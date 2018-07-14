f666ac28519cbdfcea0b20e19e76acbd2eb9b3f9
faalen <ousmanfaal2011@hotmail.com>
2018-07-14 03:01:52 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="hero-image">
+		  <div class="hero-text">
+		    <h1>I am John Doe</h1>
+		    <p>And I'm a Photographer</p>
+		    <button class="btn">Hire me</button>
+		  </div>
+		</div>
+    	<!--
+		<nav>
+			<ul>
+				<a href="#"><li>Home</li></a>
+				<a href="#"><li>About Us</li></a>
+				<a href="#"><li>Skills</li></a>
+				<a href="#"><li>Portfolio</li></a>
+				<a href="#"><li>Testimonials</li></a>
+				<a href="#"><li>Team</li></a>
+				<a href="#"><li>Contact</li></a>
+			</ul>
+		</nav>
+		-->
+	</div>
+</body>
+</html>
